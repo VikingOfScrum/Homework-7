5b43841e311a35dc80d6abd305533073253129b6
hanson-joey-7595 <49300264+hanson-joey-7595@users.noreply.github.com>
2021-05-30 13:38:39 -0700

    Created function that returns is a leap year if divisable by 4 or else not leap year

---
+++ LeapYear.py
@@ -0,0 +1,15 @@
+#Simple Program check if a year is a leap year or not
+def isLeapYear(inputYear):
+    inputYear = int(inputYear)
+    if inputYear %4 == 0:
+        return("is a Leap Year")
+    else:
+        return("is not a Leap Year")
+
+def main():
+
+    for year in range(1804, 2400):
+        print(isLeapYear(year))
+
+if __name__ == '__main__':
+    main()
