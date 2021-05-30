dc0700a3b9c52102afca34e369ef7b735823d5c9
hanson-joey-7595 <49300264+hanson-joey-7595@users.noreply.github.com>
2021-05-30 12:37:17 -0700

    Implemented test to check for values divisable by both 3 and 5

---
+++ TestFizzBuzz.py
@@ -0,0 +1,74 @@
+import pytest
+import FizzBuzz
+
+FB = FizzBuzz
+#Setting up first test to check that Fizz is a given correctly for integers divisable by 3 in values from 1 to 100
+@pytest.mark.parametrize("integer3, expectFizz", [
+    (3, "Fizz"),
+    (6, "Fizz"),
+    (9, "Fizz"),
+    (12, "Fizz"),
+    (18, "Fizz"),
+    (21, "Fizz"),
+    (24, "Fizz"),
+    (27, "Fizz"),
+    (33, "Fizz"),
+    (36, "Fizz"),
+    (39, "Fizz"),
+    (42, "Fizz"),
+    (48, "Fizz"),
+    (51, "Fizz"),
+    (54, "Fizz"),
+    (57, "Fizz"),
+    (63, "Fizz"),
+    (66, "Fizz"),
+    (69, "Fizz"),
+    (72, "Fizz"),
+    (78, "Fizz"),
+    (81, "Fizz"),
+    (84, "Fizz"),
+    (87, "Fizz"),
+    (93, "Fizz"),
+    (96, "Fizz"),
+    (99, "Fizz")
+])
+
+def test_check_for_div_3(integer3, expectFizz):
+    result = FB.checkNumber(integer3)
+    assert result == expectFizz
+
+#Checking divisable values of 5 from range of 0 to 100 are returned correctly
+@pytest.mark.parametrize("integer5, expectBuzz", [
+    (5, "Buzz"),
+    (10, "Buzz"),
+    (20, "Buzz"),
+    (25, "Buzz"),
+    (35, "Buzz"),
+    (40, "Buzz"),
+    (50, "Buzz"),
+    (55, "Buzz"),
+    (65, "Buzz"),
+    (70, "Buzz"),
+    (80, "Buzz"),
+    (85, "Buzz"),
+    (95, "Buzz"),
+    (100, "Buzz")
+])
+
+def test_check_for_div_5(integer5, expectBuzz):
+    result = FB.checkNumber(integer5)
+    assert result == expectBuzz
+
+#Checking divisable values of both 3 and 5 from range of 0 to 100 are returned correctly
+@pytest.mark.parametrize("integer35, expectFizzBuzz", [
+    (15, "FizzBuzz"),
+    (30, "FizzBuzz"),
+    (45, "FizzBuzz"),
+    (60, "FizzBuzz"),
+    (75, "FizzBuzz"),
+    (90, "FizzBuzz")
+])
+
+def test_check_for_div_3_and_5(integer35, expectFizzBuzz):
+    result = FB.checkNumber(integer35)
+    assert result == expectFizzBuzz
