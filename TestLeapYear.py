09a3c529e8e9454fd58d951227717983a60f3f7b
hanson-joey-7595 <49300264+hanson-joey-7595@users.noreply.github.com>
2021-05-30 13:53:08 -0700

    Created tests for years that are divisable by 100 should not be leap years

--- TestLeapYear.py
+++ TestLeapYear.py
@@ -1,5 +1,5 @@
 import pytest
-import TestLeapYear
+import LeapYear
 
 LY = LeapYear
 
@@ -15,8 +15,31 @@ LY = LeapYear
     (2032, "is a Leap Year"),
     (2036, "is a Leap Year"),
     (2140, "is a Leap Year"),
-    
+
 ])
 def test_div_by_4(year4, expected):
     result = LY.isLeapYear(year4)
     assert result == expected
+
+#Check years that are divisable by 100 as not being leap years
+@pytest.mark.parametrize("year100, expected100", [
+    (100, "is not a Leap Year"),
+    (200, "is not a Leap Year"),
+    (300, "is not a Leap Year"),
+    (500, "is not a Leap Year"),
+    (600, "is not a Leap Year"),
+    (700, "is not a Leap Year"),
+    (900, "is not a Leap Year"),
+    (1000, "is not a Leap Year"),
+    (1100, "is not a Leap Year"),
+    (1300, "is not a Leap Year"),
+    (1800, "is not a Leap Year"),
+    (1900, "is not a Leap Year"),
+    (2100, "is not a Leap Year"),
+    (2200, "is not a Leap Year"),
+    (2300, "is not a Leap Year"),
+])
+
+def test_div_by_100(year100, expected100):
+    result = LY.isLeapYear(year100)
+    assert result == expected100
