611f90c952daeaf3abff2de64e9e1057f495f749
hanson-joey-7595 <49300264+hanson-joey-7595@users.noreply.github.com>
2021-05-30 12:40:25 -0700

    Implemented check if value is divisiable by both 3 and 5 in checkNumber

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,13 +1,17 @@
 
 def checkNumber(i):
-    if i % 3 == 0:
+    if i % 3 == 0 and i % 5 == 0:
+        return("FizzBuzz")
+    elif i % 3 == 0:
         return("Fizz")
     elif i % 5 == 0:
         return("Buzz")
+    else:
+        return(i)
 
 def main():
     for integer in range(100):
-        checkNumber(integer)
+        print(checkNumber(integer))
 
 if __name__ == '__main__':
     main()
